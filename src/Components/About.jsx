d2a939d6888c6c81a3b440a2d428b00d7d942b2d
Avishkar Lokhande <avishkarrl23087@gmail.com>
2025-01-31 19:24:01 +0530

    Added info

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,30 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a final year computer engineering student at Vishwakarma institute of information technology. Through out my academics I have worked on various technologies like web development, cloud services, artificial intelligence and machine learning and worked on various projects where I applied these technologies and provide solution to real world problems.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C/C++",
+  "Python",
+  "Java",
+  "HTML",
+  "CSS",
+  "Javascript",
+  "React.js",
+  "Tailwind CSS",
+  "Node.js",
+  "Express.js",
+  "MongoDB",
+  "MySQL",
+  "Git",
+  "GitHub",
+  "Docker",
+  "AWS"
+
 ];
 
 /**
@@ -45,7 +56,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about coding and solving real world problems through technology and showcasing my technical skills and problem-solving ability.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,40 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Coding Assessment Platform",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      " Developed a scalable online assessment platform using the MERN stack, enabling instructors to create and assign coding tests while providing students with real‐time code evaluation powered by Judge0 API",
+    url: "  https://github.com/Avishkar-1505/Assessment-Platform",
   },
   {
-    title: "Web Development for Beginners",
+    title: "ConvoCloud",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Developed a scalable web‐based chat application using React, Node.js, Express, and MongoDB, supporting real‐time communication with Socket.io. Containerized application services with Docker, optimizing deployment and ensuring portability across environments.",
+    url: "https://github.com/Avishkar-1505/ConvoCloude",
   },
   {
-    title: "My Resume Site",
+    title: "EduStream",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "An Edtech Platform, Designed and implemented interactive and dynamic learning modules enhancing student engagement and improving learning outcomes.",
+    url: "https://github.com/Avishkar-1505/EduStream-MERN",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Resume Generation using NLP Python, BERT (Hugging Face)",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Development and integration of the resume generation pipeline, ensuring seamless processing from data input to structured output.",
+    url: "",
+  },
+  {
+    title: "Jobify",
+    description:
+      "Developed a full-stack web application using the MERN (MongoDB, Express.js, React, Node.js) stack to manage job applications. Features include user authentication, job tracking, contact management, and note-taking. Implemented responsive design for seamless usage on both desktop and mobile devices",
+    url: "https://github.com/Avishkar-1505/Jobify-MERN",
+  },
+  {
+    title: "Tetris",
+    description:
+      "Developed Classic Tetris Game in C++. Utilized the RaylibC++ GUI library to construct the GUI interface. Applied Object Oriented Programming principles to structure and implement the project. Applied design and implementation techniques to the game logic, fostering modular and scalable approach",
+    url: "https://github.com/Avishkar-1505/Tetris-Raylib-Cpp",
   },
 ];
 
@@ -61,13 +73,13 @@ const Portfolio = () => {
     <section className="padding" id="portfolio">
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+        {/* <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
             style={{ height: "90%", width: "100%", objectFit: "cover" }}
             alt={imageAltText}
           />
-        </div>
+        </div> */}
         <div className="container">
           {projectList.map((project) => (
             <div className="box" key={project.title}>
